e7b770e2e43f906a1aae4c29e3ef307f7918726f
mynameisrim <99284741+mynameisrim@users.noreply.github.com>
2023-11-13 16:49:17 +0100

    Update pipeline-rim.groovy

--- pipeline-rim.groovy
+++ pipeline-rim.groovy
@@ -7,45 +7,115 @@ pipeline {
         NEXUS_REPOSITORY = "maven-releases"
         NEXUS_CREDENTIAL_ID = "nexus-credentials"
     }
-    stages{
-        stage('Compile-package'){
-            steps{
-                script{
-                    sh 'mvn package'
+  stages {
+        stage('Building') {
+            steps {
+                script {
+                    sh 'mvn clean compile'
                 }
             }
         }
-        stage('Sonarqube Analysis'){
-            steps{
-                script{
+
+        stage('Unit Testing') {
+            steps {
+                script {
+                    sh 'mvn test'
+                }
+            }
+            post {
+                always {
+                    junit '**/target/surefire-reports/TEST*.xml'
+                }
+            }
+        }
+        stage('Sonarqube Analysis') {
+            steps {
+                script {
                     jacoco()
                     def mvnHome = tool name: 'maven-3', type: 'maven'
-                    withSonarQubeEnv('sonar'){
+                    withSonarQubeEnv('sonar') {
                         sh "${mvnHome}/bin/mvn verify sonar:sonar"
                     }
                 }
             }
         }
-        stage("Quality gate status check") {
+
+        stage("Quality Gate") {
             steps {
-                script{
+                script {
                     sleep(10)
                     timeout(time: 1, unit: 'HOURS') {
-                        def qg = waitForQualityGate()
-                        if(qg.status != 'OK') {
+                        def qg = waitForQualityGate() // Reuse taskId previously collected by withSonarQubeEnv
+                        if (qg.status != 'OK') {
                             error "Pipeline aborted due to quality gate failure: ${qg.status}"
                         }
                     }
                 }
             }
         }
-        stage("Deploying jar to Nexus Repository"){
-            steps{
-                script{
-                    nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'maven-releases', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: '', filePath: './target/achat-1.0.jar']],mavenCoordinate: [artifactId: 'achat', groupId: 'tn.esprit.rh', packaging: 'jar', version: '1']]]
+
+        stage('Building jar') {
+            steps {
+                script {
+                    def mvnHome = tool name: 'maven-3', type: 'maven'
+                    sh "${mvnHome}/bin/mvn clean package"
                 }
             }
         }
+
+//        stage("Deploying jar to Nexus Repository") {
+//            steps {
+//                script {
+//                    script {
+//                        def mvnHome = tool name: 'maven-3', type: 'maven'
+//                        def groupId = 'tn.esprit' // Replace with your project's group ID
+//                        def artifactId = 'kaddem' // Replace with your project's artifact ID
+//                        def version = '0.0.1-SNAPSHOT' // Replace with the version of your artifact
+//                        def packaging = 'jar' // Replace with the packaging type if different
+//
+//                        sh """
+//                ${mvnHome}/bin/mvn deploy
+//                -Durl=${env.NEXUS_PROTOCOL}://${env.NEXUS_URL}/repository/${env.NEXUS_REPOSITORY}/
+//                -DrepositoryId=${env.NEXUS_REPOSITORY}
+//                -DgroupId=${groupId}
+//                -DartifactId=${artifactId}
+//                -Dversion=${version}
+//                -Dpackaging=${packaging}
+//                -Dfile=target/${artifactId}-${version}.${packaging}
+//            """
+//                    }
+//                }
+//            }
+//        }
+
+        stage("publish to nexus") {
+            steps {
+                script {
+                    artifactPath = "target/kaddem-0.0.1-SNAPSHOT.jar";
+
+                    echo "*** File: ${artifactPath}, group: tn.esprit, packaging: jar, version 0.0.1-SNAPSHOT ***";
+
+                    nexusArtifactUploader(
+                            nexusVersion: NEXUS_VERSION,
+                            protocol: NEXUS_PROTOCOL,
+                            nexusUrl: NEXUS_URL,
+                            groupId: 'tn.esprit',
+                            version: '0.0.1-SNAPSHOT',
+                            repository: NEXUS_REPOSITORY,
+                            credentialsId: NEXUS_CREDENTIAL_ID,
+                            artifacts: [
+                                    // Artifact generated such as .jar, .ear and .war files.
+                                    [artifactId: 'kaddem',
+                                     classifier: '',
+                                     file      : artifactPath,
+                                     type      : 'jar']
+                            ]
+                    );
+
+                }
+            }
+        }
+      
         stage('Email Notification'){
             steps{
                 script{
